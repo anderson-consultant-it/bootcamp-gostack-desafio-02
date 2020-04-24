32f4870c0ca0e10d080f18dd8690230002ab7f09
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-04-24 11:47:43 -0400

    test buddy works

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,38 @@
 module.exports = {
-    dialect: 'postgres',
-    host: process.env.ELEPHANTSQL_HOST || 'localhost',
-    username: process.env.ELEPHANTSQL_USERNAME || 'postgres',
-    password: process.env.ELEPHANTSQL_PASSWORD || 'docker',
-    database: process.env.ELEPHANTSQL_DATABASE || 'fastfeet',
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        username: 'postgres',
+        password: 'docker',
+        database: 'gobarber',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
+    },
+    test: {
+        dialect: 'postgres',
+        host: process.env.AWS_RDS_HOST || 'localhost',
+        username: process.env.AWS_RDS_USERNAME || 'postgres',
+        password: process.env.AWS_RDS_PASSWORD || 'docker',
+        database: process.env.AWS_RDS_DATABASE || 'gobarber',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
+    },
+    production: {
+        dialect: 'postgres',
+        host: process.env.AWS_RDS_HOST || 'localhost',
+        username: process.env.AWS_RDS_USERNAME || 'postgres',
+        password: process.env.AWS_RDS_PASSWORD || 'docker',
+        database: process.env.AWS_RDS_DATABASE || 'gobarber',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
     },
 };
